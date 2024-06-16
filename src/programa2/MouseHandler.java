7d8aa358ebd86393a4cc1152a20cba3fa08effc5
NextVaso <pablocurutchet92@gmail.com>
2024-06-16 15:07:34 -0300

    agregado mouseeventlistener

--- src/programa2/MouseHandler.java
+++ src/programa2/MouseHandler.java
@@ -1,13 +1,16 @@
 package programa2;
 
+import javax.swing.event.MouseInputListener;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
-public class MouseHandler implements MouseMotionListener {
+public class MouseHandler implements MouseMotionListener, MouseListener {
     public int cursor_pos_x = 0;
     public int cursor_pos_y = 0;
+
+    public boolean clicked = false;
 
     @Override
     public void mouseDragged(MouseEvent e) {
@@ -18,6 +21,31 @@ public class MouseHandler implements MouseMotionListener {
     public void mouseMoved(MouseEvent e) {
         cursor_pos_x = e.getX()-13;
         cursor_pos_y = e.getY()-35;
-        System.out.printf("posX %d, posY %d\n",cursor_pos_x,cursor_pos_y);
+        //System.out.printf("posX %d, posY %d\n",cursor_pos_x,cursor_pos_y);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        clicked = true;
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        clicked = false;
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
